d5e55695c8b67e9f8dd9203b37c635693d7954bf
vsoch <vsochat@stanford.edu>
2016-06-27 13:49:40 -0700

    updating bogus_secrets

--- shub/bogus_secrets.py
+++ shub/bogus_secrets.py
@@ -1,7 +1,1 @@
-TURK = {
-    'host': 'mturk.com/mturk/externalSubmit',
-    'sandbox_host':'workersandbox.mturk.com/mturk/externalSubmit',
-    'app_url':'expfactory.org',
-    'debug': 1
-}
 SECRET_KEY='bogus'
