bece5cff3ba6f5f2692a4db932c9d84876bb209b
vsoch <vsochat@stanford.edu>
2016-06-30 15:04:14 -0700

    adding shub templates

--- shub/apps/shub/urls.py
+++ shub/apps/shub/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 import shub.apps.shub.views as shub_views
 
 urlpatterns = patterns('',
-    url(r'^containers$', shub_views.all_containers, name="containers")
+    url(r'^containers$', shub_views.all_containers, name="containers"),
+    url(r'^containers/new$', shub_views.new_container, name="new_container")
 )
